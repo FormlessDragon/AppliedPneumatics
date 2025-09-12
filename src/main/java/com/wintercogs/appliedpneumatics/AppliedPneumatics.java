5fce52c573852b03524c6d3b076696119f512843
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2025-09-12 13:43:22 +0800

    初始化仓库

---
+++ src/main/java/com/wintercogs/appliedpneumatics/AppliedPneumatics.java
@@ -0,0 +1,46 @@
+package com.wintercogs.appliedpneumatics;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
+
+
+@Mod(AppliedPneumatics.MODID)
+public class AppliedPneumatics
+{
+    public static final String MODID = "appliedpneumatics";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public AppliedPneumatics(IEventBus modEventBus, ModContainer modContainer)
+    {
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        LOGGER.info("AppliedPneumatics server side setup");
+    }
+}
---
+++ src/main/java/com/wintercogs/appliedpneumatics/AppliedPneumaticsClient.java
@@ -0,0 +1,26 @@
+package com.wintercogs.appliedpneumatics;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = AppliedPneumatics.MODID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = AppliedPneumatics.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+public class AppliedPneumaticsClient
+{
+    public AppliedPneumaticsClient(ModContainer container)
+    {
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event)
+    {
+        AppliedPneumatics.LOGGER.info("AppliedPneumatics client side setup");
+    }
+}

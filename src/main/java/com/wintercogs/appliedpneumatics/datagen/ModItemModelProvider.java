680cd5c48069c3dfd1eca799ce0119039be96522
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2025-09-24 16:33:15 +0800

    忘记datagen了，尴尬

--- src/main/java/com/wintercogs/appliedpneumatics/datagen/ModItemModelProvider.java
+++ src/main/java/com/wintercogs/appliedpneumatics/datagen/ModItemModelProvider.java
@@ -37,6 +37,7 @@ public class ModItemModelProvider extends ItemModelProvider
         basicItem(APItems.AMADRON_WIRELESS_TERMINAL.get());
         basicItem(APItems.AMADRON_PATTERN.get());
         basicItem(APItems.AIR_CELL_SHELL.get());
+        basicItem(APItems.MEGA_AIR_CELL_SHELL.get());
     }
 
     // 快速注册带led灯状态的存储元件模型
